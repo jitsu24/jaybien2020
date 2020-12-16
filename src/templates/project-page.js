e205f25ac0b92e9e5a24076ed31caea1928e27a6
Jay Bien-aime <jay.bienaime@gmail.com>
2020-12-15 23:08:10 -0500

    plus project pages dynamically generated from json, template built, needs small tweaks and a lil pop

--- src/templates/project-page.js
+++ src/templates/project-page.js
@@ -3,19 +3,72 @@ import { Link, graphql} from "gatsby"
 
 
 import {Layout, Menu, NavBottom} from '../components';
+import {Blob} from '../components/images';
 import SEO from "../components/seo"
+import '../styles/project-page.scss';
 
 
 
-const ProjectPage = ({props})=>{
-    console.log({props});
+const ProjectPage = ({data})=>{
+    console.log({data});
+    const {projectJson: project} = data;
+    const {title, subtitle, body, blobFill, involvement, featuredImage, projectImages, link} = project;
     return(
       <div className={`project-page`}>
-          <h1>Hello World</h1>
+          <div className="project-header">
+              <h1 className="project-header__title">{title}</h1>
+              <p className={"project-header__body"}> {body}</p>
+          </div>
+          <div className={'project-display'}>
+
+              <div className={'project-display__cell'}>
+                  <Blob className={'project-display__blob'} ></Blob>
+                <img src={`${featuredImage.publicURL}`} className={'project-display__img'}/>
+              </div>
+              <div className={'project-display__cell'}>
+                <h2 className={'project-display__subtitle'}>Involvement</h2>
+                <ul className={'project-display__list'}>
+                    {involvement.map(i => (<li>{i}</li>))}
+                </ul>
+
+                {link && (<a href={link} target="_blank">Visit</a>)}
+                
+               
+              </div>
+
+              {projectImages.map(img => (
+                <div className={'project-display__cell'}>
+                    <img src={`${img.publicURL}`} className={'display__img'}/>
+                </div>
+              ))}
+      
+               
+          </div>
       </div>
 
     )
 }
+
+export const query = graphql`
+    query($slug: String!){
+        projectJson(slug: {eq: $slug}){
+            title
+            subtitle
+            date
+            featuredImage{
+                publicURL
+            }
+            projectImages{
+                publicURL
+            }
+            blobFill
+            involvement
+            body
+            link
+        }
+    }
+    
+`
 
 
 
